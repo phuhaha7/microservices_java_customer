852a57056aa878abad0362880c4b5bd5e26f094f
Phu Ha <phuthanhha@me.com>
2022-08-14 11:39:32 -0400

    first commit

---
+++ phuservices/customer/src/main/java/com/phuha/customer/CustomerService.java
@@ -0,0 +1,12 @@
+package com.phuha.customer;
+
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+    }
+}
